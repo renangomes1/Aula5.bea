0d8fa5d2429217c697be01a0224126d9b693cfd0
renangomes1 <renangomes3902r@gmail.com>
2024-08-09 14:44:24 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,31 @@
+# # Números pares de 1-20
+
+# # for num in range(2, 21, 2):
+# #  print(num)
+
+# # # Múltiplos de 5 em 5
+# for num in range(5, 55, 5):
+#  print(num)
+
+
+
+
+# multiplos_5 = range(5, 55, 5)
+
+# soma = sum(multiplos_5)
+# print("A soma dos múltiplos de 5 até 50 é:", soma)
+
+# # Tipo de variavel 
+
+# # variavel_inteiro = 10
+# # variavel_floante = 3.14
+
+# # print("O tipo de 'variavel_inteiro' é:", type(variavel_inteiro))
+# # print("O tipo de 'variavel_floante' é:", type(variavel_floante))
+
+
+# # Mensagem de saudação
+# # print('Para prosseguir digite seu nome')
+
+# # nome = input('Nome:')
+# # print(f'Olá {nome} , Seja bem vindo')
---
+++ aula.py
@@ -0,0 +1,32 @@
+#1 
+print('Bem Vindo!')
+
+#2
+booleano = True
+print(type(booleano))
+#3
+resultado = 5.7 * 3.2
+print(resultado)
+#4
+resultado = 7//3
+print(resultado)
+#5
+resultado = 15-4
+print(resultado)
+#6
+resultado = 12//3
+print (resultado)
+#7
+numero= 7
+print(numero *2)
+#8
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+soma = num1 + num2
+print("A soma é:", soma)
+
+#9
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+email = input("Digite seu email: ")
+print(f"Nome: {nome}\nIdade: {idade}\nEmail: {email}")
