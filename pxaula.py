5543f76fdb31d8918ceb6d7728da5a24d7e7df55
renangomes1 <renangomes3902r@gmail.com>
2024-08-09 17:14:07 -0300

    Add files via upload

---
+++ pxaula.py
@@ -0,0 +1,85 @@
+
+
+
+#   -10-9-8-7-6-5-4-3-2-1
+# lista = [1,2,3,4,5,6,7,8,9,10]
+# #    0,1,2,3,4,5,6,7,8,9
+
+# soma  =  sum(lista)
+# comprimento = len(lista)
+# maior = max(lista)
+# menor = min(lista)
+
+# print(f'''soma:{soma}
+# comprimento:{comprimento}
+# menor:{menor}
+# maior:{maior}
+# ''')
+
+# lista2 = [1,2,3,4,5,6,7,200,8,9,10, 15, 150 ]
+# # organizar a lista
+# lista2.sort()
+
+# print(lista2)
+
+
+# lista  =  tuple(range(1,10))
+# print(lista)
+
+# # for n in range(1,10):
+
+# n1,n2,n3 = lista
+# print(n1, n2, n3)
+
+
+conta = (4*6//2)
+print(conta)
+
+soma = (5+7)
+print(soma)
+
+conta = (3**4)
+print(conta)
+
+soma = (8+12+15//3)
+print(soma)
+
+
+
+sub = (10-2)
+mult = (sub*5)
+print(mult)
+
+div = (27//3+5)
+print(div)
+
+print (17%4)
+
+quadrado = (9*3)
+print(quadrado*quadrado)
+
+
+raiz = 81
+r = raiz**(1//2)
+print(r)
+
+S = (3*4+20)
+print(S)
+
+print(15*2-7)
+
+print(5**3)
+
+somaa = (17+21+25)
+S = (somaa//3)
+print(S)
+
+A = (11*2+7)
+print(A)
+
+Mul = (8*3-15)
+R = (Mul/2)
+print(R)
+
+
+print(2**10)
